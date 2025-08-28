599cccd2f34fdd84fb236b0a9158170dcaa5b4b2
bmd1712 <minhduc2003tn@gmail.com>
2025-08-28 10:56:35 +0700

    sửa env

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 //todo
-Route::get('/todos', [TodoController::class, 'index']);        // Lấy danh sách todo
-Route::post('/todos', [TodoController::class, 'store']);       // Thêm mới
-Route::get('/todos/{id}', [TodoController::class, 'show']);    // Xem chi tiết
-Route::put('/todos/{id}', [TodoController::class, 'update']);  // Cập nhật
-Route::delete('/todos/{id}', [TodoController::class, 'destroy']); // Xóa
+Route::get('/todos', [TodoController::class, 'index']);       
+Route::post('/todos', [TodoController::class, 'store']);       
+Route::get('/todos/{id}', [TodoController::class, 'show']);    
+Route::put('/todos/{id}', [TodoController::class, 'update']);  
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']); 
